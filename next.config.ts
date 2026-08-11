c587e76c40b8e4a274a36d4b7ee42daf215236f8
suraj-kumar89 <suraj@bluvodigital.com>
2026-08-11 18:41:24 +0530

    Add GTM

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/get-started",
+  async rewrites() {
+    return [
+      {
+        source: "/get-started",
+        destination: "https://web-form-blush.vercel.app/get-started",
+      },
+      {
+        source: "/get-started/:path*",
+        destination:
+          "https://web-form-blush.vercel.app/get-started/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
